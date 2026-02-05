d60b5eac70ad9808ec0b4dd580e68ce08ff9c873
Martin Flischman <dev_martin@icloud.com>
2026-02-05 18:23:21 +0200

    feat: add items to cart with working event listeners

--- script.js
+++ script.js
@@ -1,12 +1,22 @@
 const menu = document.getElementById("menu");
 
+let cartItems = [];
+
 menuArray.forEach((item) => {
-  menu.innerHTML += `
-  <div class="menu-item">
-    <span>${item.emoji}</span>
-    <h3>${item.name}</h3>
-    <p>${item.ingredients.join(", ")}</p>
-    <p>Price: $${item.price}</p>
-    <button>+</button>
-  </div>`;
+  menu.insertAdjacentHTML(
+    "beforeend",
+    `
+      <div class="menu-item">
+        <span>${item.emoji}</span>
+        <h3>${item.name}</h3>
+        <p>${item.ingredients.join(", ")}</p>
+        <p>Price: $${item.price}</p>
+        <button id="btn-${item.id}">+</button>
+      </div>
+    `,
+  );
+
+  document.getElementById(`btn-${item.id}`).addEventListener("click", () => {
+    cartItems.push(item);
+  });
 });
